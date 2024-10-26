7e2478bbc650a239b1b90919d875fd896794dd58
Muhammad Abdullah Bin Sharif <happinesswhat31@gmail.com>
2024-10-26 05:40:58 +0500

    STL

---
+++ DSAcode26.cpp
@@ -0,0 +1,305 @@
+#include<iostream>
+#include<vector>
+#include<list>
+#include<deque>
+#include<stack>
+#include<queue>
+#include<map>
+#include<set>
+#include<unordered_set>
+
+// bool comparator(pair<int, int>p1 , pair<int, int> p2){
+// if(p1.second<p2.second) return true;
+// if(p1.second > p2.second) return false;
+
+// if(p1.first < p2.first) return True;
+// else return false;
+
+// }
+int main()
+{  
+
+    int n = 45;
+    long int n =14;
+    long long int n3 =15;
+
+    cout<<_builtin_popcount(n) <<endl;
+    cout<<_builtin_popcountl(n2) <<endl;
+    cout<<__builtincountll(n3) << endl;
+
+    return 0;
+
+//     string s ="abc";
+//     next_permuatation(s.begin(), s.end());
+//     cout<<max(4,5)<<min(6,10)<<endl;
+
+//     int a=5,b=10;
+//     swap(a,b);
+//     cout<<a<<endl;
+
+// vector<int> vec = {1,2,3,4,5};
+
+// reverse(vec.begin)
+// int arr[5] = {3,4,5,7,1};
+// sort(arr,arr+5);
+
+// for(int val :arr)
+// {
+//     cout<< val <<" ";
+
+// }
+// cout<<endl;
+// return 0;
+// }
+
+// vector<pair<int, int>> vec = {{3,4,5,7,1}};
+// sort(vec.begin(), vec.end());
+
+// for(int val :arr)
+// {
+//     cout<< val <<" ";
+
+// }
+// cout<<endl;
+// return 0;
+
+
+
+// multiset<int> s;
+// unordered_set<int> s;
+//     set<int> s;
+//     s.insert(1);
+//     cout<<"lower bound = " <<*(s.lower_bound(4)) <<endl;
+//     cout<<"upper bound = "<<*(s.upper_bound(4))<<endl;
+//     for(auto val: s){
+//         cout<<val<<" ";
+//     }
+//     cout<<endl;
+//     return 0;
+// }
+
+// multimap<string,int> m;//by using multimap we get four different values that we saved in our data .
+// map<string,int> m;
+// unordered_map<string, int> m;//unordered map dont have duplicate values.
+
+// m["tv"]=100;
+// m.insert({"camera",25}):
+// m.empace("camera",32);
+// m.erase("tv");
+
+// for( auto p:m){
+//     cout<<p.first<<" "<<p.second<< endl;
+// }
+// couunt<<"count=" <<m.count<<endl;
+
+// if(m.find("camera") != m.end()){
+//     cout<<"found";
+
+// }
+
+// return 0;}
+// by default data comes in ascending sorted order.
+// priority_queuue<int> q;
+// priority_queue<int, vector<int>, greator<int>> q;
+
+
+// q.push(4);
+
+     
+// queue<int> q;
+// q.push(1);
+// q.push(2);
+// q.push(3);
+
+// stack<int> s;
+// s.push(1);
+// s.push(2);
+// s.push(3);
+
+// stack<int> s2;
+
+// s2.swap(s);
+// cout<<"s size" <<s.size() <<endl;
+// cout<<s2.size<<endl;
+
+// while(!is.empty()){
+//     cout<<s.top()<<"";
+//     s.pop();
+// }
+// cout<<endl;
+
+// }
+
+
+
+
+
+// pair<int, pair<int,int>>p ={1,{'a',1}};
+// vector<pair<int, int>> vec ={{1,2}, {2,3}. {3,4}};
+
+
+// vec.push_back(4,5);//insert
+// vec.emplace_back(4,5);//inplace object creation
+// for(auto p:vec){
+//     cout<<p.first<<" "<<p.second <<endl;
+// }
+
+// cout<<p.second<<endl;
+// cout<<p.first<<endl;
+// cout<<p.second.second<<endl;
+
+
+
+
+
+//     deque<int> l;
+//     list<int> l;
+//     l.emplace_back(1);
+//     l.push_back(1);
+//     l.push_back(2);
+//     l.push_front(3);
+//     l.push_front(5);
+
+//  for(int val: l) {
+//     cout<< val << "";
+//  }
+
+//  cout<<d[2] <<endl;
+//  cout<<endl;
+
+
+//  vector<int> vec;
+//  vec.push_back(1);
+//   vec.push_back(2);
+//    vec.push_back(3);
+//       vec.push_back(4);
+//    vec.emplace_back(5);
+
+// vec.pop_back();
+
+
+// vector<int> vec(3, 10);  dynamic programming - tabulation DP[][]
+
+// vector<int> vec ={1,2,3,4,5};
+
+// cout<<<*(vec.begin())<<endl;
+
+
+// vec.insert(vec.begin()+ 2,100)
+// vec.clear();
+// print<<vec.empty()<<endl;
+// cout<<"size"<<vec.size<<endl;
+
+// vec.erase(vec.begin() + 2, vec.begin()+3 );
+//size chNGWA BUT not capacity.
+
+// vector<int> vec2(vec1);
+
+// for(int val:vec){
+//     cout<<val<<" ";
+
+// }
+
+// cout<<"VALUE AT INDEX 2"<<vec[2] <<"or"<<vec.at(2)<<endl;
+// cout<<"front"<<vec.front()<<endl;
+// cout<<"back"<<vec.back()<<endl;
+
+
+//  cout<< vec.size()  <<endl;
+//  cout <<vec.capacity()  <<endl;
+
+//FORWARD ITERATOR:
+
+// vector<int> vec = {1,2,3,4,5};
+// vector<int>:: iterator it;
+ 
+//   vector<int>::reverse_iterator it;
+//   for(it = vec.begin(); ) 
+
+
+
+// for(it =vec.begin() it != vec.end(); it++){
+//     cout<<*(it) << " ";
+// }
+// cout<<endl;
+
+
+
+
+// LIST:
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- readme.md
+++ readme.md
@@ -119,3 +119,326 @@ dec--->increasing.
     j--
   }
   
+
+  **STL:(Standard Template library)**
+
+  containers.
+  
+  iterators.
+  
+  Algorithms.
+
+  Functors.
+
+__Vector:__(dynamic,resize)
+
+vector<int>vec;
+
+__Functions:__
+o(1)
+
+.size and capacity
+push_back & pop_back
+emplace_back
+at() or []
+front and back.
+
+__Other functions:__
+o(n)
+
+vec.begin()
+
+Erase.
+insert.-->   v.insert(pos,val)
+clear.
+empty.
+
+
+Iterators:
+
+vec.begin
+vec.end     
+<!-- 
+vector<int>::iterator it;
+for(it = vec.begin(); it!=vec.end(); it++){
+  cout<<*(it)<<endl;>> -->
+<!-- <-- } -->
+
+
+
+
+**LIST**(doubly linked list)
+
+list<int> 1 ={1,2,3}
+. push_back & push_front
+. emplace_back and emplace_front
+. pop_back and pop_front
+
+
+**Deque:** double ended Queue
+
+deque<int> d ={1,2,3};
+
+push_back and push_front
+pop_back and pop_front
+emplace_back and front
+.
+
+deque have random access available
+
+
+
+**PAIR:**utility lib
+
+pair<int,int>p={3,5};
+pair<char, int> p={'a',1}
+
+
+
+**STACK:**(LIFO)
+
+.stack<int> s;
+.push,emplace
+.top
+.pop
+.size
+.empty
+.swap
+
+
+**Queue:**(FIFO)
+
+queue<int> q;
+
+push, emplace
+front
+pop
+size
+empty
+swap
+
+
+**PRIORITY QUEUE:** max heap or min heap:
+
+priority_queue<int> q;
+<!-- (largest value==highest priority) -->
+priority_queue<int, vector<int>, greator<int>> q;
+
+functor is function object fro our work in comparators.
+
+push, emplace--->o(logn)
+top-->0(1)
+pop--->o(logn)
+size
+empty
+
+**MAP:**
+
+(key,value)
+map<string,int>m;
+m[key] = value;//insert,change
+
+insert, emplace
+count
+erase
+find-->iterator and m.end()
+size,empty and erase
+
+Multimap:
+multimap<string,int> m;
+
+Unordered Map:
+its time complexity too differ from map(self balalncing tree(ologn))
+unorder(o(1))
+
+unordered_map<string, int> m;
+
+
+**SET:**
+
+set<int> s;
+
+insert,emplace
+count
+erase
+find
+size,empty, erase
+unique value and sorted order.
+unordered set: random arrranged(T.C:O(1))
+lower bound: sholud not value less than key
+upper bound: greator than key.
+
+
+**Algorithms:**
+
+Sorting:
+sort(arr,n)
+sort(arr,arr+n,greator<int>())
+sort(v.begin(),v,end())
+
+
+custom comparators:---> bool return 
+
+Reverse,
+Next permutations.
+reverse(v.begin(), v.end())
+next_permutation(v.begin(), v.end())
+swap, min ,max
+
+max and min elements:
+max_element(v.begin(), v.end())
+min_element(v.begin(),v.end())
+
+-->Binary search:
+binary_search(v.begin(), v.end(), target)
+
+-->Count Set bits:
+__builtin_popcount()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
